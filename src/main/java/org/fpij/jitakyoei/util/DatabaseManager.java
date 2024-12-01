7552c2a1408d99e3467b64e3bdb6e62799b68681
WolWolffz <gtmelis9743@hotmail.com>
2024-12-01 16:36:54 -0300

    testes appfacade

--- src/main/java/org/fpij/jitakyoei/util/DatabaseManager.java
+++ src/main/java/org/fpij/jitakyoei/util/DatabaseManager.java
@@ -2,7 +2,9 @@ package org.fpij.jitakyoei.util;
 
 import com.db4o.Db4o;
 import com.db4o.ObjectContainer;
+import com.db4o.ObjectSet;
 import com.db4o.ext.ExtObjectContainer;
+import com.db4o.query.Query;
 
 public class DatabaseManager {
 	public static final String PRODUCTION = "fpij.db";
@@ -31,5 +33,25 @@ public class DatabaseManager {
 
 	public static void setEnviroment(String env) {
 		ENVIROMENT = env;
+	}
+
+	public static void deleteall(){
+		try {
+            // Consultar todos os objetos armazenados
+            Query query = db.query();
+            query.constrain(Object.class); // Consulta todos os objetos do banco
+            ObjectSet<Object> objetos = query.execute();
+            
+            // Excluir cada objeto
+            for (Object obj : objetos) {
+                db.delete(obj);
+            }
+            
+            // Salvar alterações
+            db.commit();
+            System.out.println("Banco de dados limpo com sucesso!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 	}
 }
